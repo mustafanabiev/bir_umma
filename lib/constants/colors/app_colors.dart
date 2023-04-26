299e3d1769ab2bd6617b9ae2033daa867df9c60d
aibikearturovna <ogaiaibike4@gmail.com>
2023-04-26 17:06:42 +1000

    dobavila assets/fonst/colors,sdelala home_page

---
+++ lib/constants/colors/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/animation.dart';
+
+class AppColors {
+  static const Color white = Color(0xffffffff);
+  static const Color black = Color(0x00000000);
+  static const Color blue = Color(0xff0D96FC);
+  static const Color yellow = Color(0xffFFB900);
+}
--- lib/modules/home/view/home_view.dart
+++ lib/modules/home/view/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:bir_umma/constants/constants.dart';
 import 'package:flutter/material.dart';
 
 class HomeView extends StatelessWidget {
@@ -7,7 +8,70 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomeView'),
+        leading: const Icon(Icons.menu),
+        foregroundColor: Colors.black,
+        backgroundColor: AppColors.white,
+        elevation: 0.0,
+        centerTitle: true,
+        title: const Text(
+          'B i r  U m m a',
+          style: TextStyle(
+            fontFamily: 'Allura-Regular',
+            fontSize: 32,
+            color: AppColors.blue,
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: GridView.builder(
+              padding: const EdgeInsets.all(10),
+              itemCount: 13,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                mainAxisSpacing: 48,
+                crossAxisSpacing: 12,
+              ),
+              itemBuilder: (context, index) {
+                return Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(22),
+                  ),
+                  child: InkWell(
+                    onTap: () {},
+                    child: Stack(
+                      children: [
+                        Image.asset(
+                          'assets/images/kepin.png',
+                          fit: BoxFit.cover,
+                          height: double.infinity,
+                          width: double.infinity,
+                        ),
+                        const Positioned(
+                          right: 10,
+                          left: 10,
+                          // top: 60,
+                          bottom: 7,
+                          child: Text(
+                            'Кабыр казуу',
+                            textAlign: TextAlign.center,
+                            // 'Кепин ороо',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w700,
+                              color: AppColors.yellow,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
